fdd1ebf23239e31ea792d404ade1b844b4cda352
Rocky14683 <rocky10208@gmail.com>
2024-03-27 20:48:27 -0400

    User database stuff

---
+++ src/UserDataBase.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class UserDataBase {
+    private static ArrayList<User> users = new ArrayList<>();
+
+
+    public static User createUser(String name) {
+        String uniqueID = UUID.randomUUID().toString();
+        User newUser = new User(name, uniqueID);
+        users.add(newUser);
+        return newUser;
+    }
+
+    public static User searchUser(String name) {
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    protected static User getUser(String id) { // not suppose to be use by user
+        for (User user : users) {
+            if (user.getUniqueID().equals(id)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static void banUser(User user) {
+        users.remove(user);
+    }
+
+    public static void unbanUser(User user) {
+        users.add(user);
+    }
+
+    public static ArrayList<User> getUsers() {
+        return users;
+    }
+
+
+}
