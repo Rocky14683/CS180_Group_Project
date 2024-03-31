f513db612e81aefc8c32746f10e1b3c589137576
Rocky14683 <rocky10208@gmail.com>
2024-03-31 13:16:08 -0400

    Work on image storage
    Store image as bmp file

--- src/UserFolder/UserProfile.java
+++ src/UserFolder/UserProfile.java
@@ -1,21 +1,43 @@
 package UserFolder;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class UserProfile {
-    private String profilePicture;
+    private BufferedImage profilePic; //cache
+    private String pfpFileName;
     private String bio;
 
-    public UserProfile(String profilePicture, String bio) {
-        this.profilePicture = profilePicture;
+    public UserProfile(String pfpFileName, String bio) {
+        this.pfpFileName = pfpFileName;
         this.bio = bio;
     }
 
-    public String getProfilePicFile() {
-        return profilePicture;
+    public void setProfilePic(BufferedImage profilePic) {
+        this.profilePic = profilePic;
+        try {
+            ImageIO.write(profilePic, "bmp", new File(this.getPfpStorageDir()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getPFPFileName() {
+        return pfpFileName;
+    }
+
+    public String getPfpStorageDir() {
+        return "../Resources/" + pfpFileName;
+    }
+
+    public BufferedImage getProfilePic() {
+        return profilePic;
     }
 
     public String getBio() {
         return bio;
     }
+
 }
