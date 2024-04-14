69100b2a5dd05d08de8ca4b7c9d4ae681aba5aff
lama0608 <lucas.abreu0608@egmail.com>
2024-04-14 15:16:17 -0400

    Post and comment with actual stuff in them

--- src/DatabaseFolder/Comment.java
+++ src/DatabaseFolder/Comment.java
@@ -1,5 +1,75 @@
 package DatabaseFolder;
+import java.util.ArrayList;
+
+import UserFolder.*;
 
 public class Comment {
-    
+    private int likes;
+    private ArrayList<User> likeUsers;
+    private int dislikes;
+    private ArrayList<User> dislikeUsers;
+    private String text;
+    private User owner;
+    private String code;
+    private static int numComments = 0;
+
+    public Comment(String text, User owner) {
+        likes = 0;
+        dislikes = 0;
+        likeUsers = new ArrayList<>();
+        dislikeUsers = new ArrayList<>();
+
+        this.text = text;
+        this.owner = owner;
+
+        numComments += 1;
+        this.code = String.format("%s-%08d", owner.getUserId(), numComments);
+    }
+
+    public boolean likePost(User user) {
+        for (User u : likeUsers) {
+            if (u.equals(user)){
+                return false;
+            }
+        }
+        if (user.equals(owner)) {
+            return false;
+        }
+
+        likes += 1;
+        likeUsers.add(user);
+        return true;
+    }
+
+    public boolean dislikePost(User user) {
+        for (User u : dislikeUsers) {
+            if (u.equals(user)){
+                return false;
+            }
+        }
+        if (user.equals(owner)) {
+            return false;
+        }
+
+        dislikes += 1;
+        dislikeUsers.add(user);
+        return true;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
 }
--- src/DatabaseFolder/Post.java
+++ src/DatabaseFolder/Post.java
@@ -1,5 +1,93 @@
 package DatabaseFolder;
 
+import UserFolder.*;
+import java.util.*;
+
 public class Post {
+
+    private static int numPosts = 0;
+    private String text;    
+    private String image; 
+    private String postCode;
+    private User owner;
+    private int likes;
+    private ArrayList<User> likesUsers;
+    private int dislikes;
+    private ArrayList<User> dislikesUsers;
+    private ArrayList<Comment> comments;
+    private ArrayList<User> hiddenFrom;
+
+
+    public Post(String text, String image, User owner) {
+        this.text = text;
+        this.image = image;
+        this.owner = owner;
+        numPosts += 1;
+        this.postCode = String.format("%s-%04d", owner.getUserId(), numPosts);
+        likes = 0;
+        dislikes = 0;
+        comments = new ArrayList<>();
+        hiddenFrom = new ArrayList<>();
+        likesUsers = new ArrayList<>();
+        dislikesUsers = new ArrayList<>();
+
+    }
+
+    public boolean likePost(User user) {
+        for (User u : likesUsers) {
+            if (u.equals(user)){
+                return false;
+            }
+        }
+        if (user.equals(owner)) {
+            return false;
+        }
+
+        likes += 1;
+        likesUsers.add(user);
+        return true;
+    }
+
+    public boolean dislikePost(User user) {
+        for (User u : dislikesUsers) {
+            if (u.equals(user)){
+                return false;
+            }
+        }
+        if (user.equals(owner)) {
+            return false;
+        }
+
+        dislikes += 1;
+        dislikesUsers.add(user);
+        return true;
+    }
+
+    public boolean addComment(User user, Comment comment) {
+        comments.add(comment);
+
+        return true;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+    
+    public int getLikes() {
+        return likes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
     
 }
