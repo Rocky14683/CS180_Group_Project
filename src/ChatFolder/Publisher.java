6e6a0acb3b87762de9beb361c5664336cbf2d0c3
Rocky14683 <rocky10208@gmail.com>
2024-04-14 23:29:22 -0400

    Refactor chat

---
+++ src/ChatFolder/Publisher.java
@@ -0,0 +1,13 @@
+package ChatFolder;
+
+public class Publisher {
+    private String name;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public void publish(String message) {
+        //server -> out
+    }
+}
